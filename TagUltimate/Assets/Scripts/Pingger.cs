5968e4482041784f5e9c59a2fbdab1b9a2cc47f4
DM <dommcneill@outlook.com>
2024-08-11 19:03:20 +0100

    pigers

--- TagUltimate/Assets/Scripts/Pingger.cs
+++ TagUltimate/Assets/Scripts/Pingger.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Pingger : MonoBehaviour
 {
+	public float Force = 10;
+
 	// Start is called before the first frame update
 	void Start()
 	{
@@ -20,7 +22,9 @@ public class Pingger : MonoBehaviour
 	{
 		if (other.gameObject.GetComponent<Rigidbody>() != null)
 		{
+			Vector3 vector = other.transform.position - transform.position;
 
+			other.gameObject.GetComponent<Rigidbody>().AddForce(vector.normalized * Force);
 		}
 	}
 }
